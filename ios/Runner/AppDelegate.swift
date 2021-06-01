56eab86cec6d862df1561df258dd3c88134c3b17
compumarket <islam.kamal.dev@gmail.com>
2021-06-01 16:46:07 +0200

    apple updates 2

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,11 +8,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-      if #available(iOS 13.0, *) {
-
-         } else {
-             FirebaseApp.configure()
-         }
+  
      GMSServices.provideAPIKey("AIzaSyDH_1MBUwgEPyoiwWdjJELlRr8YXy3XAuA")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
